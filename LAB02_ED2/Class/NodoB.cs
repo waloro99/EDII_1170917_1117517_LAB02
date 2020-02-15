8e5c35936297c93367ce0ba94139718be2e32bc1
waloro <walterof1@hotmail.com>
2020-02-15 16:40:14 -0600

    create new class Nodob

---
+++ LAB02_ED2/Class/NodoB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace LAB02_ED2.Class
+{
+    internal class NodoB<T> where T : ITextoTamañoFijo
+    {
+
+    }
+}
