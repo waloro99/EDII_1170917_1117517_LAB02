64df4ca91b37a287a47abd6cbbd165d60e20bc1d
waloro <walterof1@hotmail.com>
2020-02-23 18:27:11 -0600

    commit alternativo

--- LAB02_ED2/Class/BTree_A.cs
+++ LAB02_ED2/Class/BTree_A.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LAB02_ED2.Class;
 
 namespace LAB02_ED2.Class
 {
     public class BTree_A <TKey, T> where TKey : IComparable<TKey>
     {
+
+        public NodoB<TKey, T> Raiz { get; set; }
+
+        public int Grado { get; private set; }
+
+        public int Altura { get; private set; }
 
 
 
